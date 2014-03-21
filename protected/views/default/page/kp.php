82638f0c3825ab6f4b3a87b49aeab40377b75c70
Cheshenkov Zhenia <cheshenkov@lbr.ru>
2014-03-21 14:41:07 +0300

    Edit style documentation

--- protected/views/default/page/kp.php
+++ protected/views/default/page/kp.php
@@ -1,30 +1,35 @@
 <h1>КП API</h1>
 <p>Для того чтобы начать работать с пользователями, требуется открыть http соединение по адресу <b>api.lbr.ru</b></p>
 <div class="item">
-    <h4>КП по запасным частям и <i>технике(временно не функционирует)</i></h4>
+    <!--<h4>КП по запасным частям и <i>технике(временно не функционирует)</i></h4>-->
     <div class="text">
         <p><b>URL:</b> api.lbr.ru/?r=kp&m=set</p>
-        <p>Параметры:
-        <ul>
+        <div class="h2">Основные параметры:</div>
+        <ul style="margin-top: 7px;">
             <li><span class="param">action</span> - тип передаваемых данных. Принимаемые: <b>spares</b> - Запчасти; <i><b>machine</b> - Техника(в разработке)</i></li>
-            <li><span class="param">template</span> - шаблон. Принимаемые: <b>email</b> - письмо на электронную почту, <b>print</b> - печатный шаблон(возвращает ссылку на документ) </li>
+            <li><span class="param">template</span> - шаблон. Принимаемые: <b>email</b> - письмо на электронную почту; <b>print</b> - печатный шаблон(возвращает ссылку на документ) </li>
             <li><span class="param">header</span> - Шапка. Текст либо число. 0 - скрыть шапку.</li>
             <li><span class="param">client</span> - Клиент. ФИО.</li>
             <li><span class="param">login</span> - Логин сзязанного лица(любой человек, существующий в базе <a href="http://auth.lbr.ru">Центра авторизации</a>)</li>
             <li><span class="param">f_id</span> - Филиал (Код из трех символов)</li>
             <li><span class="param">data</span> - массив с данными. Может быть множественным массивом.</li>
-            <li><br>Принимаемые параметры массива <b>data</b> для <b>action=spares</b></li>
-            <li>
-                <ul class="table">
-                    <li><span class="param">Параметр</span> <span class="type"><b>Тип</b></span> <span class="info"><b>Описание</b></span> </li>
-                    <li><span class="param">title</span> <span class="type">(string)</span> <span class="info">Название таблицы</span> </li>
-                    <li><span class="param">table</span> <span class="type">(array)</span> <span class="info">Массив со значениями таблицы</span> </li>
-                </ul>
-            </li>
         </ul>
-        <p>
-<p style="padding: 10px;">Пример генерирует таблицу вида:
-<table class="parent" style="border-collapse: collapse; margin-left: 10px; margin-bottom: 10px;" border="1" cellspacing="0" cellpadding="0" ><thead><tr><td>№</td><td>Наименование</td><td>Количество</td><td>Цена</td><td>Сумма</td></tr></thead><tbody><tr><td>1</td><td>Запчасть под номер 1</td><td>345</td><td>34</td><td>11730</td></tr><tr><td>2</td><td>Вторая ЗЧ</td><td><table class="children" width="100%" cellspacing="0" cellpadding="0"><tbody><tr><td style="border-bottom: 1px solid black;">345</td></tr><tr><td>565</td></tr></tbody></table></td><td><table class="children" width="100%" cellspacing="0" cellpadding="0"><tbody><tr><td style="border-bottom: 1px solid black;">35</td></tr><tr><td>37</td></tr></tbody></table></td><td>32980</td></tr></tbody></table>
+    </div>
+</div>
+<div class="item">
+    <h4>action = spares</h4>
+    <div class="text">
+        <p>Принимаемые параметры массива <b>data</b></p>
+        <div class="h2">Параметры:</div>
+        <ul class="table">
+            <li><span class="param">Параметр</span> <span class="type"><b>Тип</b></span> <span class="info"><b>Описание</b></span> </li>
+            <li><span class="param">title</span> <span class="type">(string)</span> <span class="info">Название таблицы</span> </li>
+            <li><span class="param">table</span> <span class="type">(array)</span> <span class="info">Массив со значениями таблицы</span> </li>
+        </ul>
+        <div class="example">
+            <div class="h2">Примеры:</div>
+            <p>1. Генерирует таблицу вида:</p>
+            <table class="parent" style="border-collapse: collapse; margin-left: 28px; margin-bottom: 10px;" border="1" cellspacing="0" cellpadding="0" ><thead><tr><td>№</td><td>Наименование</td><td>Количество</td><td>Цена</td><td>Сумма</td></tr></thead><tbody><tr><td>1</td><td>Запчасть под номер 1</td><td>345</td><td>34</td><td>11730</td></tr><tr><td>2</td><td>Вторая ЗЧ</td><td><table class="children" width="100%" cellspacing="0" cellpadding="0"><tbody><tr><td style="border-bottom: 1px solid black;">345</td></tr><tr><td>565</td></tr></tbody></table></td><td><table class="children" width="100%" cellspacing="0" cellpadding="0"><tbody><tr><td style="border-bottom: 1px solid black;">35</td></tr><tr><td>37</td></tr></tbody></table></td><td>32980</td></tr></tbody></table>
 <pre style='font-size: 11px;'>
     action = 'spares'
     data[1][table][head][0][0] = '№';
@@ -47,7 +52,6 @@
     data[1][table][body][1][3][body][1][0] = '37';
     data[1][table][body][1][4] = '32980';
 </pre>
-</p>
-    </p>
+        </div>
     </div>
 </div>
