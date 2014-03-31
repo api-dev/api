ee99e5e21abc64cea1939cbb8f6e1793ab34e450
Cheshenkov Zhenia <cheshenkov@lbr.ru>
2014-03-31 10:10:36 +0300

    test auth

--- protected/views/default/index.php
+++ protected/views/default/index.php
@@ -1,6 +1,12 @@
 <h1>Главная</h1>
 <p>
     Добро пожаловать на сайт документации по обмену данными с сайтом.
+    <?php
+    if(!$app->user->isGuest){
+        echo CHtml::link('Выход', 'http://auth.lbr.ru/logout/', array('class'=>'logout'));
+        if($app->user->checkAccess('readUser'))
+            echo CHtml::link('Администрирование', 'http://auth.lbr.ru/admin/', array('class'=>'admin'));
+    }
+    ?>
 </p>
-
 
