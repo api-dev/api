05d8080972eea61962db156d007618c803448880
Tatsyana Krylova <krilova@lbr.ru>
2014-12-15 09:39:47 +0300

    shop view

--- protected/views/default/page/shop.php
+++ protected/views/default/page/shop.php
@@ -76,12 +76,12 @@
             <li>
                 <ul class="table">
                     <li><span class="param">Параметр</span> <span class="type"><b>Тип</b></span> <span class="info"><b>Описание</b></span> </li>
-                    <li><span class="param">external_id</span> <span class="type">(integer)</span> <span class="info">Идентефикатор группы в системе учета 1С. Уникальный. Обязательный</span> </li>
+                    <li><span class="param">external_id</span> <span class="type">(string)</span> <span class="info">Идентефикатор группы в системе учета 1С. Уникальный. Обязательный</span> </li>
                     <li><span class="param">name</span> <span class="type">(string)</span> <span class="info">Название группы</span> </li>
                     <li><span class="param">inner</span> <span class="type">(array)</span> <span class="info">Дочерние группы:
                             <ul class="table">
                                 <li><span class="param">Параметр</span> <span class="type"><b>Тип</b></span> <span class="info"><b>Описание</b></span> </li>
-                                <li><span class="param">external_id</span> <span class="type">(integer)</span> <span class="info">Идентефикатор группы в системе учета. Уникальный. Обязательный</span> </li>
+                                <li><span class="param">external_id</span> <span class="type">(string)</span> <span class="info">Идентефикатор группы в системе учета. Уникальный. Обязательный</span> </li>
                                 <li><span class="param">name</span> <span class="type">(string)</span> <span class="info">Название группы</span> </li>
                                 <li><span class="param">inner</span> <span class="type">(array)</span> <span class="info">Дочерние группы ...</span></li>
                             </ul>
