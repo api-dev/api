23e99f60aebe73ce3b79a8cf0bb141d13da7071c
Tatsyana Krylova <krilova@lbr.ru>
2014-12-15 10:09:23 +0300

    style

--- protected/views/default/page/shop.php
+++ protected/views/default/page/shop.php
@@ -79,7 +79,7 @@
                     <li><span class="param">external_id</span> <span class="type">(string)</span> <span class="info">Идентефикатор группы в системе учета 1С. Уникальный. Обязательный</span> </li>
                     <li><span class="param">name</span> <span class="type">(string)</span> <span class="info">Название группы</span> </li>
                     <li><span class="param">inner</span> <span class="type">(array)</span> <span class="info">Дочерние группы:
-                            <ul class="table">
+                            <ul class="table-inner">
                                 <li><span class="param">Параметр</span> <span class="type"><b>Тип</b></span> <span class="info"><b>Описание</b></span> </li>
                                 <li><span class="param">external_id</span> <span class="type">(string)</span> <span class="info">Идентефикатор группы в системе учета. Уникальный. Обязательный</span> </li>
                                 <li><span class="param">name</span> <span class="type">(string)</span> <span class="info">Название группы</span> </li>
