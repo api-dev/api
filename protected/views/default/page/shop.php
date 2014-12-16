d036754bf52138f0d354e1acdeb0ca9d593b7fba
Tatsyana Krylova <krilova@lbr.ru>
2014-12-16 12:15:30 +0300

    shop: update for view

--- protected/views/default/page/shop.php
+++ protected/views/default/page/shop.php
@@ -6,6 +6,7 @@
     <a href="#addtr">Добавление/редактирование запчастей</a>
     <a href="#addgroup">Добавление/редактирование группы запчастей</a>
     <a href="#addcategory">Добавление/редактирование категорий</a>
+    <!--a href="#addmodelline">Добавление/редактирование модельного ряда</a-->
 </div>
 <div class="item">
     <h4><a name="addtr">Добавление/редактирование запчастей</a></h4>
@@ -38,13 +39,13 @@
         <p>Пример добавляет/редактирует двух производителей техники:</p>
         <pre style='font-size: 11px;'>
             action = 'sparepart'
-            data[1][external_id] = 'MNS0032184'
-            data[1][name] = 'Датчик весовой 375WS'
-            data[1][catalog_number] = '375WS'
+            data[0][external_id] = 'MNS0032184'
+            data[0][name] = 'Датчик весовой 375WS'
+            data[0][catalog_number] = '375WS'
             ...
-            data[2][external_id] = 'MNS0041059'
-            data[2][name] = 'Индикатор GT-460'
-            data[2][catalog_number] = 'GT-460'
+            data[1][external_id] = 'MNS0041059'
+            data[1][name] = 'Индикатор GT-460'
+            data[1][catalog_number] = 'GT-460'
         </pre>
     </div>
     <!--h4><a name="addtr">Добавление/редактирование производителя запчастей</a></h4>
@@ -95,12 +96,12 @@
         <p>Пример добавляет/редактирует группы:</p>
         <pre style='font-size: 11px;'>
             action = 'group'
-            data[1][external_id] = '3333333'
-            data[1][name] = 'Метизы'
-            data[1][inner][1][external_id] = '111111'
-            data[1][inner][1][name] = 'Болты'
-            data[1][inner][2][external_id] = '222222'
-            data[1][inner][2][name] = 'Гайки'
+            data[0][external_id] = 'MNS0004523'
+            data[0][name] = 'Метизы'
+            data[0][inner][0][external_id] = 'MNS0004524'
+            data[0][inner][0][name] = 'Болты'
+            data[0][inner][1][external_id] = 'MNS0004525'
+            data[0][inner][1][name] = 'Гайки'
         </pre>
     </div>
     <h4><a name="addcategory">Добавление/редактирование категорий</a></h4>
@@ -131,13 +132,55 @@
         <div class="h2">Примеры:</div>
         <p>Пример добавляет/редактирует категории:</p>
         <pre style='font-size: 11px;'>
-            action = 'group'
-            data[1][external_id] = '3333333'
-            data[1][name] = 'Почвообработка и посев'
-            data[1][inner][1][external_id] = '111111'
-            data[1][inner][1][name] = 'Бороны дисковые'
-            data[1][inner][2][external_id] = '222222'
-            data[1][inner][2][name] = 'Бороны пружинные'
+            action = 'category'
+            data[0][external_id] = 'MNS0004523'
+            data[0][name] = 'Почвообработка и посев'
+            data[0][inner][0][external_id] = 'MNS0004524'
+            data[0][inner][0][name] = 'Бороны дисковые'
+            data[0][inner][1][external_id] = 'MNS0004525'
+            data[0][inner][1][name] = 'Бороны пружинные'
         </pre>
     </div>
+    <!--h4><a name="addmodelline">Добавление/редактирование модельного ряда и модели</a></h4>
+    <div class="text">
+        <p><span class="param">URL</span> : api.lbr.ru/shop?m=set</p>
+        <p>Параметры:</p>
+        <ul>
+            <li><span class="param">action</span> - тип передаваемых данных. Принимаемые: <b>modelline.</b></li>
+            <li><span class="param">data</span> - массив с данными.</li>
+            <li><br>Принимаемые параметры <span class="param">data</span> при <span class="param">action=modelline:</span> </li>
+            <li>
+                <ul class="table">
+                    <li><span class="param">Параметр</span> <span class="type"><b>Тип</b></span> <span class="info"><b>Описание</b></span> </li>
+                    <li><span class="param">external_id</span> <span class="type">(string)</span> <span class="info">Идентефикатор группы в системе учета 1С. Уникальный. Обязательный</span> </li>
+                    <li><span class="param">name</span> <span class="type">(string)</span> <span class="info">Название модельного ряда</span> </li>
+                    <li><span class="param">category_id</span> <span class="type">(string)</span> <span class="info">ID категории в 1С, которой принадлежит модельный ряд</span> </li>
+                    <li><span class="param">inner</span> <span class="type">(array)</span> <span class="info">Дочерние модели:
+                            <ul class="table-inner">
+                                <li><span class="param">Параметр</span> <span class="type"><b>Тип</b></span> <span class="info"><b>Описание</b></span> </li>
+                                <li><span class="param">external_id</span> <span class="type">(string)</span> <span class="info">Идентефикатор группы в системе учета. Уникальный. Обязательный</span> </li>
+                                <li><span class="param">name</span> <span class="type">(string)</span> <span class="info">Название модели</span> </li>
+                                <li><span class="param">category_id</span> <span class="type">(string)</span> <span class="info">ID категории в 1С, которой принадлежит модель</span> </li>
+                                <li><span class="param">inner</span> <span class="type">(array)</span> <span class="info">Дочерние модели ...</span></li>
+                            </ul>
+                        </span>
+                    </li>
+                </ul>
+            </li>
+        </ul>
+        <div class="h2">Примеры:</div>
+        <p>Пример добавляет/редактирует модельного ряда и моделей:</p>
+        <pre style='font-size: 11px;'>
+            action = 'modelline'
+            data[0][external_id] = '3333333'
+            data[0][name] = '2N Сеялки'
+            data[0][category_id] = 'MNS0004523'
+            data[0][inner][0][external_id] = '111111'
+            data[0][inner][0][name] = '2N-2410'
+            data[0][inner][0][category_id] = 'MNS0004523'
+            data[0][inner][1][external_id] = '222222'
+            data[0][inner][1][name] = '2N-3010'
+            data[0][inner][1][category_id] = 'MNS0004523'
+        </pre>
+    </div-->
 </div>
