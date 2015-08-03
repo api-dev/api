b8d88c554608a1ac82e37369824d1eace7dcbb3e
Tatsyana Krylova <krilova@lbr.ru>
2015-08-03 17:54:03 +0300

    set watermark

--- protected/controllers/ShopthumbsController.php
+++ protected/controllers/ShopthumbsController.php
@@ -1,7 +1,7 @@
 <?php
 
-class ShopthumbsController extends Controller {
-
+class ShopthumbsController extends Controller 
+{
     public function actionSmall() 
     {
         set_time_limit(0);
@@ -40,6 +40,65 @@ class ShopthumbsController extends Controller {
                 $thumb->directory = $folder."m/";
                 $thumb->createThumb();
                 $thumb->save();
+            }
+        }
+    }
+    
+    public function actionWatermark() 
+    {
+        set_time_limit(0);
+        ini_set('memory_limit', '-1');
+        $folder = $_SERVER['DOCUMENT_ROOT'].'/images/shop/new/';
+        $watermarkImg = $_SERVER['DOCUMENT_ROOT'].'/images/shop/watermark/watermark_new.png';
+        $images = scandir($folder);
+        foreach ($images as $image) {
+            if (($image != '.') && ($image != '..') && (exif_imagetype($folder.$image) == IMAGETYPE_JPEG)) {
+                $image_path = $folder.$image;
+                $image = imagecreatefromjpeg($image_path);
+                // if something wrong
+                if ($image === false) {
+                    return false;
+                }
+                $size = getimagesize($image_path);
+
+                if(($size[0]>=250)||($size[1]>=250)){
+                    // create watermark
+                    $watermark = imagecreatefrompng($watermarkImg);  
+
+                    // het watermark's height/widht
+                    $watermark_width = imagesx($watermark);
+                    $watermark_height = imagesy($watermark); 
+
+                    // change watermark's size
+                    $k_w=$watermark_height/$watermark_width;
+                    $k_h=$watermark_width/$watermark_height;
+
+                    $k1=$size[0]/$watermark_width;
+                    $k2=$size[1]/$watermark_height;
+
+                    if($k1>$k2){
+                        $k=$k1;
+                        $w=$watermark_width*$k;
+                        $h=$w*$k_w;
+                    }
+                    else{
+                        $k=$k2;
+                        $h=$watermark_height*$k;
+                        $w=$h*$k_h;
+                    }
+
+                    // put watermark to image
+                    imagealphablending($image, true);
+                    imagealphablending($watermark, true);
+
+                    // create new image
+                    imagecopyresampled($image,$watermark,0,0,0,0,$w,$h,$watermark_width,$watermark_height);
+                    imagejpeg($image,$image_path);
+                    // clear memeory
+                    imagedestroy($watermark);  
+                }
+
+                imagedestroy($image);
             }
         }
     }
