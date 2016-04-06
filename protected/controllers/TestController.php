4bac05f0bf653f90437468684039f4fc4ef5732b
Tatsyana Krylova <krilova@lbr.ru>
2016-04-06 15:45:48 +0300

    test

--- protected/controllers/TestController.php
+++ protected/controllers/TestController.php
@@ -4,9 +4,20 @@ class TestController extends Controller
 {
     public function actionIndex() {
         $model = Transport::model()->findByPk(76);
+        
         echo '<pre>';
-        var_dump($model); exit;
-        //$model->rate_id = null;
-        //$model->save();
+        
+        
+        
+        $model->rate_id = null;
+        
+        
+        if(!$model->save()) {
+            print_r($model->getErrors());
+            
+        }
+        
+        var_dump($model->rate_id); 
+        exit;
     }
 }
