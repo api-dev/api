7ebf62e30b08af9331c02489fe50dbd82588873b
Tatsyana Krylova <krilova@lbr.ru>
2016-04-06 15:47:00 +0300

    del comments

--- protected/controllers/TestController.php
+++ protected/controllers/TestController.php
@@ -2,22 +2,6 @@
 
 class TestController extends Controller
 {
-    public function actionIndex() {
-        $model = Transport::model()->findByPk(76);
-        
-        echo '<pre>';
-        
-        
-        
-        $model->rate_id = null;
-        
-        
-        if(!$model->save()) {
-            print_r($model->getErrors());
-            
-        }
-        
-        var_dump($model->rate_id); 
-        exit;
-    }
+//    public function actionIndex() {
+//    }
 }
